4c37ed9c88b89194c0779fb160c853d299b9608a
manasa-lovely <manasadevipenugonda18@gmail.com>
2025-08-24 16:57:57 +0530

    navigation changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Register from "./Register.jsx";
-import { HashRouter, Routes, Route } from "react-router-dom"; // 👈 use HashRouter
+import { HashRouter, Routes, Route, Link } from "react-router-dom"; // 👈 include Link
 import OnlineJudge from "./onlineJudge.jsx";
 import Login from "./loginpage.jsx";
 import QuestionsList from "./questionsList.jsx";
@@ -10,9 +10,22 @@ import ForgotPassword from "./forgetPassword.jsx";
 import ResetPassword from "./resetPassword.jsx";
 import PrivateRoute from "./PrivateRoute.jsx";
 
+// Optional: Simple navigation bar for testing routes
+function Navigation() {
+  return (
+    <nav style={{ marginBottom: "20px" }}>
+      <Link to="/">Register</Link> |{" "}
+      <Link to="/login">Login</Link> |{" "}
+      <Link to="/forgot-password">Forgot Password</Link> |{" "}
+      <Link to="/questions">Questions</Link>
+    </nav>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HashRouter>
+      <Navigation /> {/* 👈 Add navigation bar */}
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/login" element={<Login />} />
